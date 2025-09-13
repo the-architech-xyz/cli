45518a38aa42f27a2946566536b8fe4c9a45e669
Antoinesrvt <antoine.srvt@gmail.com>
2025-09-13 12:28:14 +0200

    folder change

---
+++ src/commands/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Commands - Main Entry Point
+ * 
+ * Exports all CLI commands for The Architech
+ */
+
+export * from './new.js';
+export * from './add.js';
+export * from './scale.js';
+export * from './list-genomes.js';
